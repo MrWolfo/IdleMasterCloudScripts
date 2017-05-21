ad63c12a953e7a6c33749e9e398c5bf828e9e2ed
Omegalpha <ariellarias@gmail.com>
2017-05-21 00:53:24 -0300

    6

--- DailyRewards.js
+++ DailyRewards.js
@@ -75,6 +75,13 @@ handlers.DailyRewards_GetMyInfo = function (args)
     if(Player_DailyRewards_Data.DayCounter - Player_DailyRewards_Data.DailyRewardLastTime > 1)
     {
         Player_DailyRewards_Data.StreakCounter = 0;
+
+         var save = server.UpdateUserInternalData(
+           {
+                PlayFabId : currentPlayerId,
+                Data : {"DailyReward" :  JSON.stringify( Player_DailyRewards_Data ) } 
+           } 
+        );
     }
 
     var TimeSinceLastCheck = Date.now() - DailyRewards_Data.LastCheckTime;
