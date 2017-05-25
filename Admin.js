ad57af6c3e3e558de8b2bd9f54c5e7263464277b
Omegalpha <ariellarias@gmail.com>
2017-05-25 00:23:06 -0300

    6

--- Admin.js
+++ Admin.js
@@ -1,6 +1,11 @@
 handlers.RefreshCloudScript = function( args )
 {
-    var request = admin.GetCloudScriptRevision();
+    var url = "https://6C14.playfabapi.com/Admin/GetCloudScriptRevision";
+    var method = "post";
+    var contentBody = JSON.stringify( {} );
+    var contentType = "application/json";
+    var headers = {"X-SecretKey" : "Y3XPTHQKPUXRJRYAZAUF7AXZCQNJ1Q99KWNB41KK4UBXWD653B"};
+    var responseString =  http.request(url,method,contentBody,contentType,headers);
 
-    return { pepote: request}
+    return { pepote: responseString}
 }
