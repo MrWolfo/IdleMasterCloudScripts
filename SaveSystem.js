444f75fcf7f94c6d59d1765d9bad60c178566c24
Omegalpha <ariellarias@gmail.com>
2017-05-24 21:16:31 -0300

    a

--- SaveSystem.js
+++ SaveSystem.js
@@ -6,6 +6,80 @@
 */
 
 
+handlers.SaveInfo = function( args )
+{
+    var SAVE_NAME = args["SaveName"];
+    var DATA      = args["data"];
+
+    switch(SAVE_NAME)
+    {
+           case "GameLogic" : Save_GameLogic(); break;
+
+    }
+
+
+}
+
+handlers.LoadInfo = function( args )
+{
+    switch(SAVE_NAME)
+    {
+           case "GameLogic" : Save_GameLogic(); break;
+
+    }
+    
+}
+
+function Request_SavedGame(SaveName, DefaultObject)
+{
+    var dataRequest = server.GetUserInternalData
+                      ({
+                            PlayFabId : currentPlayerId, 
+                            Keys : [SaveName]
+                      });
+
+    //--- If Saved Data, overwritte it
+    if(dataRequest.Data.hasOwnProperty(SaveName))
+    {
+        DefaultObject = JSON.parse(dataRequest.Data[SaveName].Value) ;
+    }
+
+    return DefaultObject;
+}
+
+
+//////
+//      --------------------------------------- GAME LOGIC INFO  ----------------------------------                        
+//////
+var Data_GameLogic = 
+{
+    "LastSavedTime" : 0
+};
+
+
+function Save_GameLogic(data)
+{
+    Data_GameLogic = Request_SavedGame("GameLogic",Data_GameLogic);
+
+    Data_GameLogic.LastSavedTime = data;
+}
+
+function Load_GameLogic(data)
+{
+    Data_GameLogic = Request_SavedGame("GameLogic",Data_GameLogic);
+
+    var ReturnString = Data_GameLogic.LastSavedTime;
+
+    return ReturnString;
+}
+
+
+
+
+
+
+
+
 handlers.SetClickersData = function( args )
 {
     // the formatted string with clickers data
@@ -109,84 +183,3 @@ handlers.GetClickersData = function( args )
 }
 
 
-
-/**
- *  Set User Internal Data Wrapper for Given User  
- *  Data will be stored in a Key call GenericData as Json
- *  
- *  args Is a Dictionary key<>value to be stored in the JSON
- * 
- *  return JSON Response messageValue:"OK"
- */
-handlers.UpdateGenericPlayerData= function( args )
-{
-
-    
-    var dataRequest = server.GetUserInternalData
-                      ({
-                            PlayFabId : currentPlayerId, Keys : ["GenericData"]
-                      });
-
-
-    var CurrentData = {};
-
-    for(var i = 0; i < args["argsList"].length;  i++)
-    {
-        var argName = args["argsList"][i];
-        CurrentData[argName] = args[argName];
-    }
-
-    var updateUserInternalDataResult = server.UpdateUserInternalData(
-            {
-                PlayFabId : currentPlayerId,
-                Data : {"GenericData" :  JSON.stringify( CurrentData ) } 
-            });
-
-   return {  messageValue : "OK" }; 
-}
-
-/**
- *  RESET VOLATILE DATA (initializing) 
- *  Resets Generic Data and Also Teams Data (better for Stress)
- */
-handlers.ResetVolatileData = function (args)
-{
-
-    //---------------------      
-    var DataToSave =  {
-                                "PlayerName"    : args.PlayerUserName ,
-                                "SelectedSkin"  : "1" ,
-                                "PlayerLevel"   :  1,
-                                "Prestiges"     :  0,
-                                "LoginTimes"    :  0,
-                                "Ads"           :  "noads",
-                                "TimeSinceCreation"    :  "notime",
-                                "TotalTimeOnline"      :  "notime",
-                                "CurrentMoney"         :  "0",
-                                "ClickersDiscovered"   :  0,
-                                "QtyDiamonds"     :  "0",
-                                "QtyFragments"    :  "0",
-                                "APKVersion"      : "none"
-
-                        };
-
-    var updateUserInternalDataResult = server.UpdateUserInternalData(
-            {
-                PlayFabId : currentPlayerId,
-                Data : { 
-                    "GenericData" : JSON.stringify(DataToSave)
-                       
-                    
-                }
-   
-            }); 
-
-
-    
-
-    CreateResetPlayerTeamsData();
-
-
-    return {  messageValue : "OK"  }; 
-
-}
