f3d52d246371e455c2ea3037a36483450957027a
Omegalpha <ariellarias@gmail.com>
2017-05-25 04:32:27 -0300

    cxd

--- Admin.js
+++ Admin.js
@@ -16,11 +16,11 @@ handlers.RefreshCloudScript = function( args )
     var method = "post";
     var contentBody = JSON.stringify( {
                             "Version": 1,
-                            "Revision": data.Revision
+                            "Revision": data.data.Revision
                             } );
     var contentType = "application/json";
     var headers = {"X-SecretKey" : "Y3XPTHQKPUXRJRYAZAUF7AXZCQNJ1Q99KWNB41KK4UBXWD653B"};
     var responseString =  http.request(url,method,contentBody,contentType,headers);
 
-    return { answer : responseString };
+    return { answer : data.data.Revision };
 }
