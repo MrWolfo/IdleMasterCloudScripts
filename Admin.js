0ca8f0b9bd7ce87ff45bc503f76a89c2863b5faa
Omegalpha <ariellarias@gmail.com>
2017-05-25 04:28:17 -0300

    d

--- Admin.js
+++ Admin.js
@@ -9,7 +9,7 @@ handlers.RefreshCloudScript = function( args )
     var headers = {"X-SecretKey" : "Y3XPTHQKPUXRJRYAZAUF7AXZCQNJ1Q99KWNB41KK4UBXWD653B"};
     var responseString =  http.request(url,method,contentBody,contentType,headers);
 
-    var data = JSON.parse( responseString );
+    var data = JSON.parse( responseString ); ///--
 
     return { lala : data };
 }
