0a7725636bcaa1d68c0406c001ac59a2c78f611a
Omegalpha <ariellarias@gmail.com>
2017-07-30 02:45:13 +1200

    --

---
+++ Config_Clickers_Info.js
@@ -0,0 +1,153 @@
+var CLICKERS_INFO = 	
+{
+    "version" : 0,
+    "clickers" :
+    [
+        {
+        "Name": "KittiesClicker",
+        "IdName": "Cat",
+        "Color": "1D53A4",
+        "Info": "Feed them all!",
+        "SkillName": "Fortune Cat",
+        "SkillInfo": "Fortune Cat will pop in periodically.*Tap it and the Cat will help you doing autotaps.*More Kitties Clicker upgrades you have, more duration of the effect."
+        },
+        {
+        "Name": "PieClicker",
+        "IdName": "Bakery",
+        "Color": "E03B5A",
+        "Info": "Pie Tower of Infinite Floors.*One tap, one Sugar grain.",
+        "SkillName": "Power CupCake",
+        "SkillInfo": "A Power CupCake will appear in screen periodically.*Tap it to Multiply your Profits during a couple of secs.*More Pie Clicker upgrades you have, more duration of the effect."
+        },
+        {
+        "Name": "FantasyClickTap",
+        "IdName": "Fantasy",
+        "Color": "5E52A2",
+        "Info": "Magic, Dragons, Swords and Dungeons without Limit.",
+        "SkillName": "Kitty Ranger",
+        "SkillInfo": "Party Teams are a must in Fantasy Worlds.*Now a Kitty Ranger appears each time you tap FortuneCat, adding more taps! Kitty Ranger is more powerful with each boost you did on FantasyClickTap"
+        },
+        {
+        "Name": "CaffeineClicker",
+        "IdName": "Caffeine",
+        "Color": "00B6E1",
+        "Info": "Sleep if for the weaks",
+        "SkillName": "Flavor Hesitation",
+        "SkillInfo": "A Coffee Card will pop in periodically.*Tap it to select between 3 hidden Coffee Card Flavors.*Each Flavor will accelerate the time for a given duration."
+        },
+        {
+        "Name": "Zombie Waves",
+        "IdName": "Zombie",
+        "Color": "CD2026",
+        "Info": "Waves and Waves of Zombies to Kills in this infinite clicker.",
+        "SkillName": "Zombies Horde",
+        "SkillInfo": "Each some time, a horde of Zombies will approache you! Kill many as you can (tap them) to earn money!"
+        },
+        {
+        "Name": "HeroesTapTap",
+        "IdName": "Heroes",
+        "Color": "4AB648",
+        "Info": "Defeat the evil monsters, once after other. Yep, there is no way to win, just more and more enemies..",
+        "SkillName": "Heroe Tap Punch",
+        "SkillInfo": "Unlocks Critical Strike Taps.*On each tap, you have a chance of doing a Heroe Tap Punch that earns lots of money.*Cute Tip: Fortune Cat will do critical taps too!"
+        },
+        {
+        "Name": "Endless Racing",
+        "IdName": "Racing",
+        "Color": "CD2026",
+        "Info": "The Endless Hours of Le Mens. Each Day a New Driver takes the seat.",
+        "SkillName": "Endless Racing",
+        "SkillInfo": "Go to Play Games tab to play Endless Racing.  Earn Multipliers and Diamonds!"
+        },
+        {
+        "Name": "IdleMagnate",
+        "IdName": "Magnate",
+        "Color": "CD2026",
+        "Info": "Buy, Buy Now!!!!!",
+        "SkillName": "Flash Sales",
+        "SkillInfo": "A Flash Sale notification will promp each certain time.*Upgrades will be in discount while the effect is active.*More IdleMagnate upgrades, better discounts. "
+        },
+        {
+        "Name": "VideoTubers",
+        "IdName": "VideoTubers",
+        "Color": "C5D92C",
+        "Info": "Reach the overflow number of visits and break the internet!",
+        "SkillName": "Please Suscribe",
+        "SkillInfo": "Unlocks VideoTuber helper.*VideoTuber will collect 1 diamond for each Rewarded Video you watch.*Now they are even more rewarding!"
+        },
+        {
+        "Name": "Maths Clicker",
+        "IdName": "Maths",
+        "Color": "CD2026",
+        "Info": "Exponentially think about Exponential Problem",
+        "SkillName": "Do the Maths",
+        "SkillInfo": "All the Clickers gain +5% speed for each Boost this clicker has done (max of 100%)"
+        },
+        {
+        "Name": "Sport Champions",
+        "IdName": "SportChampions",
+        "Color": "F8971D",
+        "Info": "An Endless rain of Goals, Dunks and HomeRuns.",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        },
+        {
+        "Name": "Social Incremental",
+        "IdName": "Social",
+        "Color": "CD2026",
+        "Info": "A Social Network without limits of Friends. ",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        },
+        {
+        "Name": "GameDev Clicker",
+        "IdName": "GameDev",
+        "Color": "CD2026",
+        "Info": "If Only Games were created by Idling.. Mix well with Caffeine Clicker.",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        },
+        {
+        "Name": "Music Loops",
+        "IdName": "Music",
+        "Color": "CD2026",
+        "Info": "Musical Progressions that elevate your soul and makes your brain mutate.",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        },
+        {
+        "Name": "Galaxy Jumpers",
+        "IdName": "Galaxy",
+        "Color": "CD2026",
+        "Info": "The Domination of the whole Universe would take forever.",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        },
+        {
+        "Name": "Evolution Mixes",
+        "IdName": "Evolution",
+        "Color": "CD2026",
+        "Info": "Combining genes you can create really funny creatures.",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        },
+        {
+        "Name": "Martial Legends",
+        "IdName": "Martial",
+        "Color": "F8971D",
+        "Info": "No matter how much you train, you can always be better.",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        },
+        {
+        "Name": "Idle of Idles",
+        "IdName": "IdleofIdles",
+        "Color": "F9EC23",
+        "Info": "A micraclous inception. Play an Idle of Clickers inside an Idle of Clickers!",
+        "SkillName": "NONE",
+        "SkillInfo": "No Skill Yet"
+        }
+    ]
+
+
+};
---
+++ Config_Clickers_Stats.js
@@ -0,0 +1,122 @@
+
+/// This Config var Stores the Game Information for Clickers (multipliers , stats, etc)
+
+
+var CLICKERS_STATS = 	
+{
+    "version" : 0,
+    "clickers" :
+    [
+        {
+        "IdName": "Cat",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "500",
+        "Speed": "10"
+        },
+        {
+        "IdName":  "Bakery",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "4.30E+03",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Fantasy",
+        "Class": TYPE_HARDCORE,
+        "BaseCost": "1.60E+04",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Caffeine",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "4.70E+04",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Zombie",
+        "Class": TYPE_HARDCORE,
+        "BaseCost": "2.30E+05",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Heroes",
+        "Class": TYPE_MIXED,
+        "BaseCost": "1.30E+06",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Racing",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "1.30E+08",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Magnate",
+        "Class": TYPE_HARDCORE,
+        "BaseCost": "1.30E+10",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "VideoTubers",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "1.30E+12",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Maths",
+        "Class": TYPE_HARDCORE,
+        "BaseCost": "1.30E+15",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "SportChampions",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "1.30E+17",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Social",
+        "Class": TYPE_MIXED,
+        "BaseCost": "1.30E+20",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "GameDev",
+        "Class": TYPE_HARDCORE,
+        "BaseCost": "1.30E+23",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Music",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "1.30E+26",
+        "Speed": "1.5",
+        "Color": "CD2026"
+        },
+        {
+        "IdName": "Galaxy",
+        "Class": TYPE_HARDCORE,
+        "BaseCost": "1.30E+30",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Evolution",
+        "Class": TYPE_CASUAL,
+        "BaseCost": "1.30E+34",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "Martial",
+        "Class": TYPE_HARDCORE,
+        "BaseCost": "1.30E+40",
+        "Speed": "1.5"
+        },
+        {
+        "IdName": "IdleofIdles",
+        "Class": TYPE_MIXED,
+        "BaseCost": "1.30E+48",
+        "Speed": "1.5"
+        }
+    ]
+
+
+};
