00e8268036235ce07bdefb76375bf01e441aad30
Omegalpha <ariellarias@gmail.com>
2017-07-30 03:21:20 +1200

    xx

--- ConfigFiles.js
+++ ConfigFiles.js
@@ -330,8 +330,9 @@ handlers.GetDataForKey = function (args)
 
     var DATA_BY_KEY = 
     {
-        "Clickers_InfoData"  : JSON.stringify( CLICKERS_INFO ),
-        "Clickers_StatsData" : JSON.stringify( CLICKERS_STATS )
+        "Clickers_InfoData"   : JSON.stringify( CLICKERS_INFO ),
+        "Clickers_StatsData"  : JSON.stringify( CLICKERS_STATS ),
+        "Clickers_BoostData" : JSON.stringify( CLICKERS_BOOST_CONFIG )
     };
 
     var serverData = DATA_BY_KEY[requestDataKey];
