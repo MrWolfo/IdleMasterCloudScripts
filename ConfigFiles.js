9153e4a62b0d0315fe049e1a4a4fa78010ec574f
Omegalpha <ariellarias@gmail.com>
2017-08-03 22:14:41 +1200

    dfdfd

--- ConfigFiles.js
+++ ConfigFiles.js
@@ -20,8 +20,26 @@ handlers.GetDataForKey = function (args)
 
     var serverData = DATA_BY_KEY[requestDataKey];
     var serverDataHash = GetHashByHand(serverData);
+
+
+    return Get_CSVFile_FromWeb();
+
     
     return (serverDataHash == requestDataHash) ? "OK" : serverData;
+}
+
+
+function Get_CSVFile_FromWeb()
+{
+    var url = "https://www.dropbox.com/s/zkyna8i5c15cl8q/Minions_Data.csv?dl=1";
+    var method = "post";
+    var contentBody = "";
+    var contentType = "application/json";
+    var headers = {};
+    var responseString =  http.request(url,method,contentBody,contentType,headers); 
+
+    return responseString;
+
 }
 
 
