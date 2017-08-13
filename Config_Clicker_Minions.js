9048180902b3dd1668fe461f4f73ce18b795541b
Omegalpha <ariellarias@gmail.com>
2017-08-14 08:38:55 +1200

    +54

---
+++ Config_Clicker_Minions.js
@@ -0,0 +1,354 @@
+var MINION_SKILLS = 
+{
+    AUTOCOLLECT : 0,
+    SKILL_UNLOCKER : 1,
+    CLASS_IMPROVER : 2,
+    ALL_PROFITS    : 3,
+    ALL_SPEED      : 4,
+    SKIN_IMPROVER  : 5,
+    FRAGMENTS_IMPROVER : 6,
+    POWERCAKE_IMPROVER : 7,
+    FRAGMENT_MINIONS_POWER : 8,
+    CASUAL_POWER : 9,
+    HARDCORE_POWER : 10,
+    TAP_POWER      : 11,
+    BETTER_NATS    : 12,
+    FORTUNE_CAT_QTY : 13,
+    BETTER_ONLINE   : 14,
+    BETTER_OFFLINE  : 15,
+    BETTER_SPACEGULL : 16,
+    BETTER_COFFEE    : 17,
+    BETTER_TEAMPOINTS : 18,
+    CRITICAL_TAP_CHANCE : 19,    
+    BETTER_CRITICAL     : 20
+};
+
+
+var CLICKERS_MINIONS_CONFIG = 	
+{
+    "randomId" : "OPERTUZO",
+    "version" : 0,
+    "minions" :
+    [
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "02",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "03",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        {
+            "numSubfix" : "01",
+            "clicker"   : "Bakery",
+            "Name"      : "Brandon Ramses",
+            "Skill"     : MINION_SKILLS.AUTOCOLLECT,
+            "Quote"     : "You better clean your kitchen",
+            "Base"      : "1.1",
+            "Increment" : "0.1"
+        },
+        
+    ],
+    "fragments" :
+    [
+
+    ]
+}
